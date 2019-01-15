fcd597162638466062395014dce15e10385a2212
Jasper Kroes <J.Kroes-1@student.tudelft.nl>
2019-01-15 11:48:14 +0100

    bar statement

--- D.java
+++ D.java
@@ -1,12 +1,19 @@
 class D{
 
+	/**
+	* Je mag alleen fris drinken als je jonger bent dan 16.
+	* Je mag alleen fris + bier drinken als je jonger bent dan 18.
+	* J emag alles drinken als je 18 of ouder bent.
+	*/
 	public static void main(String[] args){
 		System.out.println("Joe");
-		int a = 23;
-		if(a == 12){
-			System.out.println("a is 12");			
+		int age = 23;
+		if(age < 16){
+			System.out.println("je alleen fris drinken");			
+		} else if (age < 18){
+			System.out.println("je mag alleen bier en fris drinken");
 		} else {
-			System.out.println("a is: " + a);
+			System.out.println("je mag alles drinken");
 		}
 	}
 
