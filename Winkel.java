94eedb28fde0d064b7abc4da271e9a62a6e85fa9
Jasper Kroes <J.Kroes-1@student.tudelft.nl>
2019-01-15 14:40:21 +0100

    added winkel

---
+++ Winkel.java
@@ -0,0 +1,75 @@
+import java.util.*;
+/*
+winkel met menu van 3 items
+
+item 1 is €1
+item 2 is €2
+item 3 is €3
+
+door winkelen totdat ze niet meer willen
+*/
+
+public class Winkel{
+
+	static LinkedList<Product> items = new LinkedList<Product>();
+
+	public static void main(String[] args){		
+
+		Product p1 = new Product("brood",1);
+		Product p2 = new Product("melk", 2);
+		Product p3 = new Product("vlees", 3);
+		items.add(p1);
+		items.add(p2);
+		items.add(p3);
+		
+		Scanner sc = new Scanner(System.in);
+		int totaal = 0;
+
+		doorwinkelen(sc, totaal);
+	
+		sc.close();	
+
+	}
+
+	static void showItems() {
+		int itemnumber = 1;
+		for(Product p : items) {
+			System.out.println(itemnumber + ". " + p.toString());
+			itemnumber++;
+		}
+	}
+
+	static void doorwinkelen(Scanner sc, int totaal){
+		System.out.println("Wilt u (nog) wat kopen? (1 = ja, 2 = nee)");
+		if(sc.nextInt() == 1) {
+			showItems();			
+			System.out.println("Wat wilt u kopen?");
+			int keuze = sc.nextInt();
+			totaal += items.get(keuze - 1).price;
+			doorwinkelen(sc,totaal);
+
+		} else {
+			showtotal(totaal);
+		}
+	}
+
+	static void showtotal(int totaal){
+		System.out.println("Uw totaal is: " + totaal + " euro.");
+	}
+
+}
+
+class Product{
+
+	private String name;
+	public int price;
+
+	public Product(String naam, int prijs){
+		this.name = naam;
+		this.price = prijs;
+	}
+
+	public String toString(){
+		return name + "\t" + price + " euro";
+	}
+}
