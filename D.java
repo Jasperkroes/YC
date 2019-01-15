9772bf416616f0031b7d3d11169e57f07ce05b53
Jasper Kroes <J.Kroes-1@student.tudelft.nl>
2019-01-15 11:41:06 +0100

    removed all added if else statement

--- D.java
+++ D.java
@@ -2,17 +2,12 @@ class D{
 
 	public static void main(String[] args){
 		System.out.println("Joe");
-		int a;
-		a = 3;
-		System.out.println(a);
-		String b = "boss";
-		System.out.println(b);
-		char s = 105;
-		System.out.println(s);
-		s--;
-		System.out.println(s);
-		--s;
-		System.out.println(s);	
+		int a = 23;
+		if(a == 12){
+			System.out.println("a is 12");			
+		} else {
+			System.out.println("a is: " + a);
+		}
 	}
 
 }
