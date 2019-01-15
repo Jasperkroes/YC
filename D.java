e19ef4b9fd0887b2ea929b5ab9d834b79107b4f9
Jasper Kroes <J.Kroes-1@student.tudelft.nl>
2019-01-15 13:18:17 +0100

    Added person with constructor and getters

--- D.java
+++ D.java
@@ -2,30 +2,31 @@ import java.util.Scanner;
 
 class D{
 
-	/**
-	* Opdracht:
-	* Je mag alleen fris drinken als je jonger bent dan 16.
-	* Je mag alleen fris + bier drinken als je jonger bent dan 18.
-	* Je mag alles drinken als je 18 of ouder bent.
-	*/
 	public static void main(String[] args){
-		
-		//default age
-		int age = 15;
-		
-		System.out.println("Geef je leeftijd op: ");
+		Persoon persoona = new Persoon("man","Jasper");
+		System.out.println("Persoon a heet: " + persoona.getNaam());
+		Persoon persoonb = new Persoon("man","Peter");
+		System.out.println("Persoon b heet " + persoonb.getNaam() + " en is een " + persoonb.getGeslacht());
 
-		Scanner sc = new Scanner(System.in);
-		age = sc.nextInt();
-		sc.close();
+	}
 
-		if(age < 16){
-			System.out.println("Je mag alleen fris drinken.");			
-		} else if(age < 18){
-			System.out.println("Je mag alleen bier en fris drinken.");
-		} else {
-			System.out.println("Je mag alles drinken.");
-		}
+}
+
+class Persoon{
+	private String geslacht;
+	private String naam;
+
+	public Persoon(String ges, String n){
+		geslacht = ges;
+		naam = n;
+	}
+
+	public String getNaam() {
+		return this.naam;
+	}
+
+	public String getGeslacht() {
+		return this.geslacht;
 	}
 
 }
